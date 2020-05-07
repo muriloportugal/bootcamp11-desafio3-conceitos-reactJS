db29dc4993f2faa9402f35aa7d8014317c8fc71f
Murilo Portugal <murilo.portugal@gmail.com>
2020-05-06 23:40:17 -0300

    Finalizado Desafio 3

---
+++ README.md
@@ -0,0 +1,38 @@
+<p align="center">
+  <img  src="./assets/BootCamp-GoStack.png">
+</p>
+<h3 align="center"> Desafio 3 - Conceitos do ReactJS </h3>  
+<p>  
+<p>  
+
+## :rocket: Sobre:  
+Neste desafio é proposto continuar a aplicação que iniciamos no [**desafio 2**](https://github.com/muriloportugal/bootcamp11-desafio2-conceitos-node.js) desenvolvendo uma interface simples em **React** para adicionar e remover os repositórios do portifólio.
+Colocamos em prática alguns conceitos, e funcionalidades do **React** como:  
+
+- Componentização;
+- Estado e Imutabilidade;
+- Propriedades;
+
+[Detalhes do desafio](https://github.com/Rocketseat/bootcamp-gostack-desafios/tree/master/desafio-conceitos-reactjs)  
+
+## :checkered_flag: Como testar:  
+:warning: **IMPORTANTE:**  
+Por se tratar de uma continuação do [**desafio 2**](https://github.com/muriloportugal/bootcamp11-desafio2-conceitos-node.js) é necessário que este esteja rodando em seu computador para que ao executar este desafio o front-end consiga se conectar no back-end criado no [**desafio 2**](https://github.com/muriloportugal/bootcamp11-desafio2-conceitos-node.js).  
+
+Neste projeto foi utilizado o yarn, então caso ainda não tenha, siga este [guia parar instalação](https://classic.yarnpkg.com/en/docs/install) do mesmo.  
+Faça o download ou o clone deste repositório para o seu computador.  
+```bash
+git clone https://github.com/muriloportugal/bootcamp11-desafio3-conceitos-reactJS.git
+```
+Entre na pasta "bootcamp11-desafio3-conceitos-reactJS" e rode o comando abaixo para que sejam instaladas as dependências:
+```bash
+yarn 
+```
+Depois de instalar as dependências, certifique-se que o back-end criado no desafio 2 esteja executando conforme mencionado acima, agora pode executar o comando abaixo e no seu navegador você já vai conseguir abrir este projeto em **http://localhost:3000/**.  
+```bash
+yarn start
+```
+---
+## :memo: Licença
+
+Esse projeto está sob a licença MIT. Veja o arquivo [LICENSE](LICENSE) para mais detalhes.
--- src/App.js
+++ src/App.js
@@ -1,28 +1,65 @@
-import React from "react";
+import React, { useState, useEffect }from "react";
 
 import "./styles.css";
 
+import api from './services/api';
+
 function App() {
+  
+  const [ repositories, setRepository ] = useState([]);
+  // When the page is loaded, useEffect send a get request to 
+  // localhost:3333/repositories and populate the 'const repositories'
+  // with the response data.
+  useEffect(
+    () => {
+      api.get('repositories').then( response => {setRepository(response.data)});
+    },
+    [ ]);
+
   async function handleAddRepository() {
-    // TODO
+    //Use axios to send a post request to localhost:3333/repositories with
+    // the new repository data in the body. When the response return
+    // insert that data into 'setRepository' respecting the React immutability
+    api.post('repositories',
+    {
+      title: `Novo repositório ${Date.now()}`,
+      url: "https://github.com/muriloportugal",
+      techs: ["Nodejs","ReactJS"],
+      likes:0
+    }).then( response => {setRepository([...repositories,response.data])});
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    // Send a delete request with the id of the repository, and when the response
+    // returns verify if the status is 204, proving the repository was already
+    // deleted in our back-end, then we can delete from 'const repositories'
+    // and recreate it with 'setRepository' respecting the React immutability
+    api.delete(`repositories/${id}`).then( response =>{
+      if(response.status===204){
+        const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+        repositories.splice(repositoryIndex,1);
+        setRepository([...repositories])
+      }
+    });
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {
+          //return each element in repositories array and create the list at HTML page
+          // each element has its on button to remove.
+          repositories.map(repository => (
+            <li key={repository.id}> {repository.title}
+              <button onClick={() => handleRemoveRepository(repository.id)}>
+                Remover
+              </button>
+            </li>
+          ))
+        }
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
       </ul>
-
+      
       <button onClick={handleAddRepository}>Adicionar</button>
     </div>
   );
